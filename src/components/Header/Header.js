fda4c24305343d993862cbb5918ecd5e9d2f7a62
shubhkr8 <shubham.kumarsingh8@gmail.com>
2024-02-01 11:38:07 +0530

    links adds

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,26 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
       <Navbar bg="primary" data-bs-theme="dark" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">Resoo</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/">Resoo</Link>
+          </Navbar.Brand>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/rfqack">Acknowledge</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/rfqsubmit">Submit</Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
