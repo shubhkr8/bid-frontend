ce9f98cb6242946dbdae02c0666c6340c4d915ac
shubhkr8 <shubham.kumarsingh8@gmail.com>
2024-03-02 02:26:33 +0530

    login component

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import InputBox from "../../components/InputBox/InputBox";
+import Header from "../../components/Header/Header";
+
+const intialLoginData = {
+  usr_name: "",
+  pswrd: "",
+};
 
 const Login = () => {
+  const [loginData, setLoginData] = useState(intialLoginData);
+  const handleInputChange = (fieldName, value) => {
+    setLoginData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleLoginSubmit = () => {
+    console.log(loginData);
+    setLoginData(intialLoginData);
+  };
+
   return (
-    <div className="loginsignup">
-      <div className="loginsignup-container">
-        <h1>Login</h1>
-        <div className="loginsignup-fields">
-          <input type="text" placeholder="user Name" />
-          <input type="password" placeholder="Password" />
+    <>
+      <Header />
+      <div className="Login">
+        <div className="Login__container">
+          <h1>Login</h1>
+          <form className="Login__form">
+            <InputBox
+              label="USER NAME"
+              id="usr_name"
+              type="text"
+              value={loginData.usr_name}
+              onChange={(e) => handleInputChange("usr_name", e.target.value)}
+            />
+            <InputBox
+              label="PASSWORD"
+              id="pswrd"
+              type="password"
+              value={loginData.pswrd}
+              onChange={(e) => handleInputChange("pswrd", e.target.value)}
+            />
+          </form>
+          <button className="Login__button" onClick={() => handleLoginSubmit()}>
+            Login
+          </button>
         </div>
-        <button>Continue</button>
       </div>
-    </div>
+    </>
   );
 };
 
