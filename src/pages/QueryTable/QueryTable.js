2a68a30c85c2b53ba80fbb67245cf8733b5cbe20
shubhkr8 <shubham.kumarsingh8@gmail.com>
2024-03-19 11:25:34 +0530

    updated to render

--- src/pages/QueryTable/QueryTable.js
+++ src/pages/QueryTable/QueryTable.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
-import { AgGridReact } from "ag-grid-react";
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-alpine.css";
-import axios from "axios";
-import "./QueryTable.css";
-import Loader from "../../loader/Loader";
-import { COLUMNDEFS } from "../../utils/Constant";
-
-// const localurl = "http://localhost:5000/api/ack-data";
+import React, { useState, useEffect, useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import axios from 'axios';
+import './QueryTable.css';
+import Loader from '../../loader/Loader';
+import { COLUMNDEFS } from '../../utils/Constant';
+import { renderApiAck } from '../../utils/apiEndPoints';
 
 const QueryTable = () => {
   const [rowData, setRowData] = useState([]);
@@ -17,20 +16,18 @@ const QueryTable = () => {
     setIsLoading(true);
     const fetchData = async () => {
       try {
-        const result = await fetchDataFromApi(
-          "https://giant-cyan-camel.cyclic.app/api/ack-data"
-        );
+        const result = await fetchDataFromApi(renderApiAck);
         setRowData(result);
         setIsLoading(false);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
         setIsLoading(false);
       }
     };
     fetchData();
   }, []);
 
-  const rowSelectionType = "multiple";
+  const rowSelectionType = 'multiple';
   const onSelectionChanged = (event) => {
     console.log(event);
   };
@@ -45,21 +42,21 @@ const QueryTable = () => {
     if (gridApiRef.current) {
       gridApiRef.current.exportDataAsCsv();
     } else {
-      console.error("Grid API is not initialized.");
+      console.error('Grid API is not initialized.');
     }
   };
 
   return (
     <div>
-      <div className="table__top">
-        <button className="export-button" onClick={() => onExportClick()}>
+      <div className='table__top'>
+        <button className='export-button' onClick={() => onExportClick()}>
           Export
         </button>
       </div>
       {isLoading && <Loader />}
       <div
-        className="ag-theme-alpine"
-        style={{ width: "100%", height: "86vh" }}
+        className='ag-theme-alpine'
+        style={{ width: '100%', height: '86vh' }}
       >
         <AgGridReact
           columnDefs={COLUMNDEFS}
---
+++ src/utils/apiEndPoints.js
@@ -0,0 +1,21 @@
+//local
+export const localApiAck = 'http://localhost:5000/api/ack-data';
+export const localApiAckForm = 'http://localhost:5000/api/acknowledge-form';
+export const localApiFormNo = 'http://localhost:5000/api/form-no';
+
+//Render
+export const renderApiAck = 'https://resoo-backend.onrender.com/api/ack-data';
+export const renderApiAckForm =
+  'https://resoo-backend.onrender.com/api/acknowledge-form';
+export const renderApiFormNo = 'https://resoo-backend.onrender.com/api/form-no';
+export const renderApiLogin = 'https://resoo-backend.onrender.com/api/login';
+export const renderApiSubmit =
+  'https://resoo-backend.onrender.com/api/submit-form';
+
+//Cyclic
+export const cyclicApiAck = 'https://giant-cyan-camel.cyclic.app/api/ack-data';
+export const cyclicApiAckForm =
+  'https://giant-cyan-camel.cyclic.app/api/acknowledge-form';
+export const cyclicApiFormNo =
+  'https://giant-cyan-camel.cyclic.app/api/form-no';
+export const cyclicApiLogin = 'https://giant-cyan-camel.cyclic.app/api/login';
