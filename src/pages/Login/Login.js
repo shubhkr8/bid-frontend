d6f6c47896849c5ccfa40d43667991937dbaa45c
Anyash-17 <mailtoanyash1706@gmail.com>
2024-04-01 14:29:48 +0530

    emailer

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,11 +6,15 @@ import axios from 'axios';
 import Loader from '../../loader/Loader';
 import { useUserContext } from '../../App';
 import { renderApiLogin } from '../../utils/apiEndPoints';
+import emailjs from '@emailjs/browser';
 
 const intialLoginData = {
   usr_name: '',
   pswrd: '',
 };
+const serviceID = 'service_amiicyu';
+const templateId = 'template_weylh1n';
+const publicId = 'FOeBHDkgsZ4ZHObIp';
 
 const Login = () => {
   const [loginData, setLoginData] = useState(intialLoginData);
@@ -37,6 +41,22 @@ const Login = () => {
       setIsLogin(true);
       setRole(role);
       setIsLoading(false);
+      emailjs
+        .send(
+          serviceID,
+          templateId,
+          {
+            to_email: 'mailtoanyash1706@gmail.com',
+            from_name: loginData.usr_name,
+          },
+          publicId
+        )
+        .then((response) => {
+          console.log('Email sent successfully', response);
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error);
+        });
       navigate('/');
     } catch (error) {
       console.log('Error submitting form:', error);
@@ -48,27 +68,27 @@ const Login = () => {
   };
 
   return (
-    <div className='Login'>
-      <div className='Login__container'>
+    <div className="Login">
+      <div className="Login__container">
         <h1>Login</h1>
         {isLoading && <Loader />}
-        <form className='Login__form'>
+        <form className="Login__form">
           <InputBox
-            label='USER NAME'
-            id='usr_name'
-            type='text'
+            label="USER NAME"
+            id="usr_name"
+            type="text"
             value={loginData.usr_name}
             onChange={(e) => handleInputChange('usr_name', e.target.value)}
           />
           <InputBox
-            label='PASSWORD'
-            id='pswrd'
-            type='password'
+            label="PASSWORD"
+            id="pswrd"
+            type="password"
             value={loginData.pswrd}
             onChange={(e) => handleInputChange('pswrd', e.target.value)}
           />
         </form>
-        <button className='Login__button' onClick={() => handleLoginSubmit()}>
+        <button className="Login__button" onClick={() => handleLoginSubmit()}>
           Login
         </button>
       </div>
